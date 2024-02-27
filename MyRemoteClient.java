712dcef17c1e0473420ac8b6385ee7d156066f83
Maissa Dridi <maissadridi1@gmail.com>
2024-02-27 14:45:57 +0100

    client

--- MyRemoteClient.java
+++ MyRemoteClient.java
@@ -3,7 +3,8 @@ import java.rmi.Naming;
 public class MyRemoteClient {
     public static void main(String[] args) {
         try {
-            MyRemoteInterface remoteService = (MyRemoteInterface) Naming.lookup("//localhost/MyRemoteServer");
+            MyRemoteInterface remoteService = (MyRemoteInterface) Naming
+                    .lookup("C:\\Users\\Maissa\\Desktop\\rmi\\rmi-app\\MyRemoteInterface.java");
             String result = remoteService.convertString("hello");
             System.out.println("Result: " + result);
         } catch (Exception e) {
